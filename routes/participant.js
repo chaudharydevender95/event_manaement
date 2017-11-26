97f70d67b14fc105917cf5bea60334dcbf41a5c8
chaudharydevender95 <33262773+chaudharydevender95@users.noreply.github.com>
2017-11-26 23:05:08 +0530

    Update participant.js

--- routes/participant.js
+++ routes/participant.js
@@ -21,4 +21,12 @@ router.post('/comment_event/:id',passport.isAuthenticated, function(req,res,next
     
 })
 
+router.get('/filtered/:title/:organiser',passport.isAuthenticated,function(req,res,next){
+    Event.find({$and:[{title:req.params.title},{organiser:req.params.organiser}]},function(err,result){
+        if(err) res.json(err);
+        else res.json(result);
+    })
+})
+
+
 module.exports = router;
